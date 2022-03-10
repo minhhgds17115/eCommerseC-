868c80b1331e0d596a5e428eab4c3903c1438679
minhhgds17115 <82482747+minhhgds17115@users.noreply.github.com>
2022-03-10 17:08:56 +0700

    chuyển từ VB sang C#. thêm dữ liệu

--- eCommerse/Controllers/HomeController.cs
+++ eCommerse/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using eCommerse.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using eCommerse.Models;
+
 namespace eCommerse.Controllers
 {
     public class HomeController : Controller
     {
+        eCommerseASMEntities objeCommerseASMEntities = new eCommerseASMEntities();
         public ActionResult Index()
         {
-            return View();
+            HomeModel objHomeModel = new HomeModel();
+            objHomeModel.ListCategory = objeCommerseASMEntities.Categories.ToList();
+
+            objHomeModel.ListProduct = objeCommerseASMEntities.Products.ToList();
+            return View(objHomeModel);
         }
 
         public ActionResult About()
