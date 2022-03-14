273f0ad3ab8a5d7d6086fe7dc0c30143d641ce82
minhhgds17115 <82482747+minhhgds17115@users.noreply.github.com>
2022-03-14 17:17:34 +0700

    Add category va product detail need images

---
+++ eCommerse/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using eCommerse.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerse.Controllers
+{
+    public class CategoryController : Controller
+    {
+        eCommerseASMEntities objeCommerseASMEntities = new eCommerseASMEntities();
+        // GET: Category
+
+        public ActionResult Index()
+        {
+            var lstCategory = objeCommerseASMEntities.Categories.ToList();
+            return View(lstCategory);
+        }
+    }
+}
