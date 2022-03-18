4cce101e0e4490670a7918714c6ca38404c3a0aa
minhhgds17115 <82482747+minhhgds17115@users.noreply.github.com>
2022-03-18 17:49:10 +0700

    fix font, link category/ product

---
+++ eCommerse/Models/CategoryModel.cs
@@ -0,0 +1,15 @@
+﻿using eCommerse.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eCommerse.Models
+{
+    public class CategoryModel
+    {
+        public List<Category> ListCategory;
+        public List<brand> ListBrands;
+        public List<Product> ListProduct;
+    }
+}
